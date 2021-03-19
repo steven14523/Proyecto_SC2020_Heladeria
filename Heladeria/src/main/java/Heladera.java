14c08422c9c638f074abc539b0ec6587dd2381c6
steven14523 <darguello00876@ufide.ac.cr>
2021-03-18 20:03:12 -0600

    18/3

---
+++ Heladeria/src/main/java/Heladera.java
@@ -0,0 +1,11 @@
+//opcion
+public class Heladera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
